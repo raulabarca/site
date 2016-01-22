5fc306db1e4848933f78c3cb9879f739ffdba30e
Raúl Abarca <raulabarka@gmail.com>
2016-01-22 21:08:18 +0100

    editado

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     historyApiFallback = require('connect-history-api-fallback'),
     stylus = require('gulp-stylus'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    inject = require('gulp-inject'),
+    wiredep = require('wiredep').stream,
     nib = require('nib');
 
 // Servidor web de desarrollo
@@ -31,11 +35,44 @@ gulp.task('html', function() {
     .pipe(connect.reload());
 });
 
+// Busca errores en el JS y nos los muestra por pantalla
+gulp.task('jshint', function(){
+  return gulp.src('./app/scripts/**/*.js')
+  .pipe(jshint('.jshintrc'))
+  .pipe(jshint.reporter('jshint-stylish'))
+  .pipe(jshint.reporter('fail'));
+});
+
+// Busca en las carpetas de estilos y javascript los archivos que hayamos creado
+// para inyectarlos en el index.html
+gulp.task('inject', function() {
+  var sources = gulp.src(['./app/scripts/**/*.js', './app/stylesheets/**/*.css']);
+  return gulp.src('index.html', {cwd: './app' })
+    .pipe(inject(sources, {
+      read: false,
+      ignorePath: './app'
+    }))
+    .pipe(gulp.dest('./app'));
+});
+
+// Inyecta las librerias que instalemos vía Bower
+gulp.task('wiredep', function() {
+  gulp.src('./app/index.html')
+    .pipe(wiredep({
+      directory: './app/lib'
+    }))
+    .pipe(gulp.dest('./app'));
+});
+
 // Vigila cambios que se produzcan en el código
 // y lanza las tareas relacionadas
 gulp.task('watch', function() {
   gulp.watch(['./app/**/*.html'], ['html']);
   gulp.watch(['./app/stylesheets/**/*.styl'], ['css']);
+  gulp.watch(['./app/scripts/**/*.js', './gulpfile,js'], ['jshint']);
+  gulp.watch(['./app/stylesheets/**/*.styl'], ['css', 'inject']);
+  gulp.watch(['./app/scripts/**/*.js', './gulpfile.js'], ['jshint', 'inject']);
+  gulp.watch(['./bower.json'], ['wiredep']);
 });
 
-gulp.task('default', ['server', 'watch']);
+gulp.task('default', ['server', 'inject', 'wiredep', 'watch']);
